b1da9e6ba3ecef0b58cb98d5ed4ab64868c609a7
Nikola Radovic <nikola.radovic@devtech.local>
2019-08-05 15:04:08 +0200

    initial commit

---
+++ backend/src/resolvers/Mutation.js
@@ -0,0 +1,12 @@
+const Mutations = {
+    createDog(parent, args, cotx, info ){
+        global.dogs = global.dogs || []
+        //create a dog
+        const newDog = {name:args.name};
+        global.dogs.push(newDog)
+        return newDog;
+        
+    }
+};
+
+module.exports = Mutations;
---
+++ frontend/components/Meta.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+
+const Meta = ()=>(
+    <Head>
+     <meta charSet="UTF-8"/>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+     <link rel="shortcut_icon" href="/static/favicon.png" />
+     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+     <title>Sick Fits!</title>
+    </Head>
+)
+
+export default Meta
